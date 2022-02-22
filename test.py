e54e27fc9e60d0a97215b81ba2acddc98478621b
payambarneanderthal <99961787+payambarneanderthal@users.noreply.github.com>
2022-02-22 22:14:39 +0330

    Update test.py

--- test.py
+++ test.py
@@ -1,19 +1,14 @@
 # SECURITY 
-
 import requests
-
 import time
+
 
 target = input("enter number >>> ")
 
 url = "https://web.rubika.ir"
 
 while True:
-
-time.slerp(3)
-
+time.sleep(3)
 pyload ={"cellphone": target}
-
 u = requests.post(url, data=pyload)
-
 print(be fuck raft)
